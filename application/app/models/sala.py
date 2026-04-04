00bacd5ef325f962fc3c87cc453044c0d73a8cbe
evimariia <evilam.scarneiro@gmail.com>
2026-04-04 15:30:25 -0300

    Feat(backend): criação da função de busca de sala a partir do ID do dispositivo(ESP32)

---
+++ application/app/models/sala.py
@@ -0,0 +1,14 @@
+from database.connection import get_connection
+
+def buscar_sala_por_dispositivo(id_dispositivo):
+    db_conn = get_connection()
+    cursor = db_conn.cursor()
+
+    busca_aluno = "SELECT id_sala FROM tb_salas WHERE id_dispositivo = %s"
+    cursor.execute(busca_aluno, (id_dispositivo,))
+    sala = cursor.fetchone()
+
+    cursor.close()
+    db_conn.close()
+
+    return sala
