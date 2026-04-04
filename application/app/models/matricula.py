6e016f2592f4562c01dd982989b0b7412197ef72
evimariia <evilam.scarneiro@gmail.com>
2026-04-04 20:36:21 -0300

    Fix(backend): correção do nome da coluna na query

--- application/app/models/matricula.py
+++ application/app/models/matricula.py
@@ -4,7 +4,7 @@ def buscar_aluno_por_ra(ra_aluno):
     db_conn = get_connection()
     cursor = db_conn.cursor()
 
-    busca_aluno = "SELECT id_matricula FROM tb_matriculas WHERE ra_aluno = %s"
+    busca_aluno = "SELECT id_matricula_turma FROM tb_matriculas WHERE ra_aluno = %s"
     cursor.execute(busca_aluno, (ra_aluno,))
     aluno = cursor.fetchall()
 
@@ -17,7 +17,7 @@ def validar_matricula(ra_aluno, id_turma):
     db_conn = get_connection()
     cursor = db_conn.cursor()
 
-    busca_matricula = "SELECT id_matricula FROM tb_matriculas WHERE ra_aluno = %s AND id_turma = %s"
+    busca_matricula = "SELECT id_matricula_turma FROM tb_matriculas WHERE ra_aluno = %s AND id_turma = %s"
     cursor.execute(busca_matricula, (ra_aluno, id_turma))
     matricula = cursor.fetchone()
 
