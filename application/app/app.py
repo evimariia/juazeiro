c2928bc609d699385c6edb4672e13086d78419e0
evimariia <evilam.scarneiro@gmail.com>
2026-04-26 18:13:53 -0300

    config(flask): Configurações iniciais do Flask e definição das rotas para registro de presença

--- application/app/app.py
+++ application/app/app.py
@@ -1,8 +1,18 @@
 from datetime import datetime
-import flask
+from flask import Flask
 from application.database.connection import get_connection
 
-app = flask.Flask(__name__)
+app = Flask(__name__)
+# app.debug = True
+
+# # Import routes to register them
+from application.app.routes.presenca_routes import auth_presenca_rota
+
+app.register_blueprint(auth_presenca_rota)
+
+@app.route('/')
+def home():
+    return "Hello, World!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
